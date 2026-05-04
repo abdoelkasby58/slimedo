a6e1adfd4807b11096e198c35866f049ddc61348
abdoelkasby58 <abdoelkasby58@example.com>
2026-05-04 16:00:27 +0300

    update node

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,16 @@
 import { fileURLToPath, URL } from "node:url";
-
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
-import tailwindcss from "@tailwindcss/vite";
-
-// غير الاسم ده باسم الريبو بتاعك
 const repoName = "slimedo";
-
-export default defineConfig(({ command }) => ({
-  base: `/${repoName}/`, // ✅ مهم جدًا لـ GitHub Pages
-
-  plugins: [
-    vue(),
-    vueJsx(),
-    tailwindcss(),
-    command === "serve" ? vueDevTools() : null,
-  ].filter(Boolean),
-
+// https://vite.dev/config/
+export default defineConfig({
+  base: `/${repoName}/`,
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-
-  build: {
-    target: "es2020",
-    cssCodeSplit: true,
-    minify: "esbuild",
-    chunkSizeWarningLimit: 800,
-
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            if (id.includes("@iconify/vue")) {
-              return "icons";
-            }
-            if (
-              id.includes("vue") ||
-              id.includes("vue-router") ||
-              id.includes("pinia")
-            ) {
-              return "vue";
-            }
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
-}));
+});
